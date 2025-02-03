0e51e3f7f6a41cf8b877d24ecfa69ee7d2438027
HYPERPC <HYPERPC@DESKTOP-BIIOI1I>
2025-02-03 18:56:43 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 383);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(394, 26);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async Task fklo(string asd)
+        {
+            var words = new Dictionary<string, string>()
+            {{"Телеграм", "telegram"},
+                {"Telegram", "telegram"},
+                {"ВК", "vkontakte"},
+                {"Вк","vkontakte" },
+                {"Vk","vkontakte" },
+                {"VK", "vkontakte"},
+                {"VKontakte", "vkontakte"},
+                {"Vkontakte", "vkontakte"},
+                {"Вконтакте","vkontakte"},
+                {"ВKонтакте","vkontakte"},
+                {"Discord","discord"},
+                {"Дискорд" , "discord"},
+                {"YouTube","youtube"},
+                {"Youtube","youtube"},
+                {"Ютуб","youtube"},
+                {"WhatsApp","whatsapp"},
+                {"Whatsapp","whatsapp"},
+                {"Ватсапп","whatsapp"},
+                {"Вацапп","whatsapp"},
+                {"Instagram","instagram"},
+                {"Инстраграм","instagram"},
+                {"Mail.ru","mail-ru"},
+                {"Майл.ру","mail-ru"},
+                {"Майл ру","mail-ru"},
+                {"Одноклассники","odnoklassniki"},
+                {"Viber","viber"},
+                {"Вайбер","viber"},
+                {"Sberbank","sberbank"},
+                {"Сбербанк онлайн","sberbank"},
+                {"Сбербанк Онлайн","sberbank"},
+                {"Сбербанк","sberbank"},
+                {"Sberbank online","sberbank"},
+                {"Sberbank Online","sberbank"},
+                {"Steam","steam"},
+                {"Стим","steam"},
+                {"TikTok","tiktok"},
+                {"Тикток","tiktok"},
+                {"ТикТок","tiktok"},
+                {"Tiktok","tiktok"},
+                {"Госуслуги","gosuslugi"},
+                {"Gosuslugi","gosuslugi"},
+                {"Т-Банк","tbank"},
+                {"T-Bank","tbank"},
+                {"Т Банк","tbank"},
+            };
+
+            if (!words.TryGetValue(asd, out var sai))
+            {
+                label3.Visible = false;
+                label1.Visible = false;
+                label4.Visible = false;
+                label2.Text = "Не найдено";
+            }
+            else
+            {
+                string url = ($"https://downdetector.su/{sai}");
+                label2.Text = url;
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    ParseHtml(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: {response.StatusCode}");
+                }
+            }
+        }
+        void ParseHtml(string html)
+        { 
+        //Капэц
+        string asa = "";
+        string ass = "";
+        string assa = "";
+        string h2 = "";
+        string lol = "";
+        label3.Text = "";
+            label1.Text = "";
+            chart1.Text = "";
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+        document.LoadHtml(html);
+            var carNodes = document.DocumentNode.SelectNodes("//span[@class = 'region']"); // Замените XPath на подходящий
+        var Nodes = document.DocumentNode.SelectNodes("//label[span]");
+        var caNodes = document.DocumentNode.SelectNodes("//span[@class = 'cause']");
+        var H2 = document.DocumentNode.SelectSingleNode("/html/body/section[2]/h2").InnerText;
+        Series series = new Series();
+            chart1.Series.Clear();
+            foreach(var ser in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            if (carNodes != null)
+            {
+                label3.Visible = true;
+                label1.Visible = true;
+                label4.Visible = true;
+                foreach (var Node in H2)
+                {
+                    label4.Text = H2;
+                }
+                foreach (var Node in Nodes)
+                {
+                asa = Node.InnerText;
+                label1.Text += asa + "\n";
+
+                }
+                label1.Text += "\n";
+                for(int i = 0; i < 5; i++)
+                {
+                    var Node = carNodes[i];
+                    var Node1 = Nodes[i];
+                    ass = Node.InnerText;
+                    ass.TrimEnd('%');
+                    string newline = ass.TrimEnd('%');
+                    bool s = int.TryParse(newline, out int res);
+                    series = chart1.Series.Add(Node1.InnerText);
+                    chart1.Series[Node1.InnerText].Points.Add(res);
+                }
+                foreach (var Node in carNodes)
+                {
+                  ass = Node.InnerText;
+                  label3.Text += ass + "\n";
+                }
+                label1.Text += "\n";
+                foreach (var Node in caNodes)
+                {
+                    assa = Node.InnerText;
+                    label3.Text += assa;
+                }
+
+            }
+            else
+            {
+                label4.Visible = false;
+                label1.Visible = false;
+                label3.Visible = false;
+                label2.Text = "Ничего не найдено";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            await fklo(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
